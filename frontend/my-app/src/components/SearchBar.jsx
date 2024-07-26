243e04a8e9dea2ba428e414df06a212c1cdc6a92
bassel331 <basselbassel28@gmail.com>
2024-07-27 02:23:27 +0300

    searchbar

---
+++ frontend/my-app/src/components/SearchBar.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+import "./SearchBar.css";
+
+export const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+  
+    fetch("http://localhost:3030/universities")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((json) => {
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value.toLowerCase())
+          );
+        });
+        console.log("Filtered results:", results);
+        setResults(results);
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  };
+  
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  }; 
+ 
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
