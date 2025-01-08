842ef1c888d85840f0dd29e78a8b2b8325406af5
bessam <bessam@sumerge.com>
2025-01-08 03:28:12 +0200

    filter and search done

---
+++ backend/Routes/universitiesRoute.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const { checkAuthenticated } = require('../Middleware/middleWare');
+const University = require('../Modules/UniversitySchema.js');
+
+router.get('/', checkAuthenticated,async (req, res) => {
+    try {
+      const universities = await University.find({});
+      res.json(universities);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+router.get('/filter', async (req, res) => {
+  try {
+    const { major, sortBy, order } = req.query;
+
+    if (!major) {
+      return res.status(400).json({ error: "Major is required for filtering." });
+    }
+
+    const query = { 'majorsAvailable.name': { $regex: major, $options: 'i' } };
+  
+    let universities = await University.find(query);
+    
+    if (sortBy === 'tuitionFees') {
+      for (let i = 0; i < universities.length; i++) {
+        const sortOrder = order === 'desc' ? -1 : 1;
+        const filteredMajors= universities[i].majorsAvailable.filter(majorName => majorName.name === major)
+        universities[i].majorsAvailable=await filteredMajors
+     }
+     universities = universities.sort({ 'name': sortOrder });
+    }
+    if (sortBy === 'name') {
+        const sortOrder = order === 'desc' ? -1 : 1;
+        universities = universities.sort({ 'name': sortOrder });
+      }
+    const results=await universities; 
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Search universities by name
+router.get('/search', checkAuthenticated, async (req, res) => {
+  try {
+    const { name, sortBy, order } = req.query;
+
+    if (!name) {
+      return res.status(400).json({ error: "Name is required for searching." });
+    }
+
+    const query = { name: { $regex: name, $options: 'i' } };
+
+    let universities = University.find(query);
+
+    if (sortBy === 'name') {
+        const sortOrder = order === 'desc' ? -1 : 1;
+        universities = universities.sort({ 'name': sortOrder });
+      }
+
+    const results = await universities;
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -6,29 +6,17 @@ const path=require('path');
 const app= express();
 const fs = require('fs');
 const Users=require("./Modules/UserSchema.js");
-const University = require('./Modules/UniversitySchema.js');
-const bcrypt = require('bcrypt')
-const passport = require('passport')
 const session = require('express-session');
 const flash = require('express-flash')
 const methodOverride = require('method-override')
 const cors = require('cors');
-// var favicon = require('serve-favicon');
-//const multer =require('multer');
-// const nodemailer = require('nodemailer');
-// const sgMail = require('@sendgrid/mail');
 require('dotenv').config();
-const initializePassport = require('./passport-config.js')
-initializePassport(passport ,
-  async email=>await Users.findOne({ Email: email }),
-  async id=>{await Users.findOne({ _id: id })})
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(methodOverride('_method'))
 app.set('view engine','ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.set('Views', path.join(__dirname, 'Views'));
 app.use(express.urlencoded({ extended: false }));
 app.use(flash())
 app.use(session({
@@ -36,13 +24,22 @@ app.use(session({
   resave:false,
   saveUninitialized:false
 }))
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const initializePassport = require('./passport-config.js')
+initializePassport(passport ,
+  async email=>await Users.findOne({ Email: email }),
+  async id=>{await Users.findOne({ _id: id })}
+  )
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(cors()); 
-// app.use((req, res, next) => {
-//   res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
-//   next();
-// });
+
+const { checkAuthenticated, checkNotAuthenticated } = require('./Middleware/middleWare');
+
+
+app.use('/', require('./Routes/authRoute'));
+app.use('/universities', require('./Routes/universitiesRoute'));
 
 
 const servernum=1990;
@@ -62,165 +59,10 @@ app.listen(PORT, () => {
     const { email } = loginData;
     const user = await Users.findOne({ Email: email })
     const {First_Name} =user
-    // const universities = [
-    //   {
-    //     name: 'Example University 1',
-    //     details: 'A prestigious university known for its excellent programs.',
-    //     foundationYear: 1890,
-    //     majorsAvailable: [
-    //       {
-    //         name: 'Computer Science',
-    //         departments: ['Artificial Intelligence', 'Software Engineering'],
-    //         tuitionFees: 20000,
-    //         admissionScores: {
-    //           ig: 85,
-    //           sat: 1200,
-    //           governmentalEducation: 80
-    //         }
-    //       },
-    //       {
-    //         name: 'Business Administration',
-    //         departments: ['Marketing', 'Finance', 'Human Resources'],
-    //         tuitionFees: 18000,
-    //         admissionScores: {
-    //           ig: 82,
-    //           sat: 1150,
-    //           governmentalEducation: 78
-    //         }
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     name: 'Example University 2',
-    //     details: 'An innovative university with a focus on research.',
-    //     foundationYear: 1950,
-    //     majorsAvailable: [
-    //       {
-    //         name: 'Biotechnology',
-    //         departments: ['Genetics', 'Bioprocessing'],
-    //         tuitionFees: 25000,
-    //         admissionScores: {
-    //           ig: 88,
-    //           sat: 1250,
-    //           governmentalEducation: 85
-    //         }
-    //       },
-    //       {
-    //         name: 'Mechanical Engineering',
-    //         departments: ['Thermodynamics', 'Robotics'],
-    //         tuitionFees: 22000,
-    //         admissionScores: {
-    //           ig: 87,
-    //           sat: 1230,
-    //           governmentalEducation: 83
-    //         }
-    //       }
-    //     ]
-    //   }
-    // ];
-    
-    // University.insertMany(universities)
      res.render('home.ejs',{name: First_Name})
   })  
-
-  app.get('/login',checkNotAuthenticated,(req,res)=>{
-    res.render('login.ejs')
-  }) 
- 
-  app.post('/login', checkNotAuthenticated, (req, res, next) => {
-    passport.authenticate('local', (err, user, info) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.redirect('/login');
-        }
-        req.logIn(user, (err) => {
-            if (err) {
-                return next(err);
-            } 
-            req.session.loginData = req.body;
-            res.redirect('/')
-        });
-    })(req, res, next);
-});
+  
 
 
-  app.get('/register',checkNotAuthenticated,(req,res)=>{
-    res.render('register.ejs')
-  })   
 
-  app.post('/register',checkNotAuthenticated,async (req,res,next)=>{
-    
-    const { firstname, lastname, email, phone, usertype, password, confirmpassword, sendmail } = req.body;
-    if (password !== confirmpassword) {
-      res.redirect('/register')
-  }
-  else{
-    try{
-    const hashedpass=await bcrypt.hash(req.body.password,10)
-    await Users.insertMany({
-      First_Name: firstname,
-      Last_Name: lastname, 
-      Email: email,
-      Phone: phone,
-      UserType: usertype,
-      Password: hashedpass,
-      Send_Mail: sendmail === 'on'
-  });
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-        return next(err);
-    }
-    if (!user) {
-        return res.redirect('/login');
-    }
-    req.logIn(user, (err) => {
-        if (err) {
-            return next(err);
-        } 
-        // Now you can access req.body and perform additional actions
-        req.session.loginData = req.body;
-        res.redirect('/')
-         // Send req.body or redirect to another route as needed
-    });
-})(req, res, next);
-   } 
-   catch (error) {
-    
-   res.redirect('/register')
-}
-  }
-  }) 
-
-  app.delete('/logout', (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        res.redirect('/login');
-    });
-});  
-
-app.get('/universities',checkAuthenticated ,async (req, res,next) => {
-  try {
-    const universities = await University.find({}); // Fetch only the 'name' field
-    res.json(universities);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-  function checkAuthenticated(req,res,next){
-    if(req.isAuthenticated()){
-      return next()
-    }
-    res.redirect('/login')
-  }
-
-  function checkNotAuthenticated(req,res,next){
-    if(req.isAuthenticated()){
-      return res.redirect('/')
-    }
-    next()
-  } 
+  
